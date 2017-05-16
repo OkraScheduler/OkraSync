4ae93d2cf241120eab2ef048d87ff5ab11de4d91
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2017-05-16 14:27:57 -0300

    Using streams on index creator

--- src/test/java/okra/EnsureIndexesTest.java
+++ src/test/java/okra/EnsureIndexesTest.java
@@ -32,12 +32,11 @@ public class EnsureIndexesTest extends OkraBaseContainerTest {
 
     @Test
     public void ensureIndexesTest() {
-
-        DefaultOkraItem item = new DefaultOkraItem();
+        final DefaultOkraItem item = new DefaultOkraItem();
         item.setRunDate(LocalDateTime.now());
 
         getDefaultOkra().schedule(item);
-        IndexCreator.ensureIndexes((OkraSimple<DefaultOkraItem>) getDefaultOkra(), getDefaultMongo(), "okraSimpleTests", "okraSimple");
-    }
 
+        IndexCreator.ensureIndexes(getDefaultOkra(), getDefaultMongo(), "okraSimpleTests", "okraSimple");
+    }
 }
